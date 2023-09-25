a5131f14794ac42a8fe2e4120f8bfd624a05667b
Olga Okuneva <133260595+olya06@users.noreply.github.com>
2023-09-25 22:30:52 +0300

    dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,25 +1,46 @@
 -- Сколько у нас пользователей заходят на сайт (уникальных - distinct) в день
-select date_trunc('day', visit_date) visit_date, count(/*distinct*/ visitor_id) from sessions
+select
+    date_trunc('day', visit_date) as visit_date,
+    count(/*distinct*/ visitor_id)
+from sessions
 group by 1
-order by 1
+order by 1;
 
 -- Какие каналы их приводят на сайт? Хочется видеть по дням/неделям/месяцам
-select source, count(*), date_trunc('day', visit_date) visit_date from sessions s -- по дням
+-- по дням
+select
+    source,
+    count(*),
+    date_trunc('day', visit_date) as visit_date
+from sessions
 group by 1, 3
-order by visit_date
+order by visit_date;
 
-select source, count(*), date_trunc('week', visit_date) visit_date from sessions s -- по неделям
+-- по неделям
+select
+    source,
+    count(*),
+    date_trunc('week', visit_date) as visit_date
+from sessions
 group by 1, 3
-order by visit_date
+order by visit_date;
 
-select source, count(*), date_trunc('month', visit_date) visit_date from sessions s  -- по месяцам
+-- по месяцам
+select
+    source,
+    count(*),
+    date_trunc('month', visit_date) as visit_date
+from sessions
 group by 1, 3
-order by visit_date
+order by visit_date;
 
 -- Сколько лидов к нам приходят?
-select date_trunc('day', created_at), count(1) from leads l
+select
+    date_trunc('day', created_at),
+    count(*)
+from leads
 group by 1
-order by 1
+order by 1;
 
 -- Какая конверсия из клика в лид? А из лида в оплату?
 -- Далее используем витрину aggregate_last_paid_click
@@ -93,43 +114,52 @@ advertising AS (
         null AS purchases_count,
         daily_spent AS total_cost
     FROM ya_ads
-), big_tab as 
-(
+),
+
+big_tab AS (
+    SELECT
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        date(visit_date) AS visit_date,
+        sum(visitors_count) AS visitors_count,
+        sum(total_cost) AS total_cost,
+        sum(leads_count) AS leads_count,
+        sum(purchases_count) AS purchases_count,
+        sum(revenue) AS revenue
+    FROM advertising
+    GROUP BY
+        visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign
+    ORDER BY
+        revenue DESC NULLS LAST,
+        visit_date ASC,
+        visitors_count DESC,
+        utm_source ASC,
+        utm_medium ASC,
+        utm_campaign ASC
+)
+
 SELECT
     utm_source,
-    utm_medium,
-    utm_campaign,
-    date(visit_date) AS visit_date,
-    sum(visitors_count) AS visitors_count,
-    sum(total_cost) AS total_cost,
-    sum(leads_count) AS leads_count,
-    sum(purchases_count) AS purchases_count,
-    sum(revenue) AS revenue
-FROM advertising
-GROUP BY
-    visit_date,
-    utm_source,
-    utm_medium,
-    utm_campaign
-ORDER BY
-    revenue DESC NULLS LAST,
-    visit_date ASC,
-    visitors_count DESC,
-    utm_source ASC,
-    utm_medium ASC,
-    utm_campaign asc
-    ) 
-    select
-    utm_source,
-    round(sum(leads_count) * 100.00 / sum(visitors_count), 2) as visitor_from_lead,
-    case
-        when sum(coalesce(leads_count, 0)) = 0 then 0.00 else
-            round(sum(coalesce(purchases_count, 0))* 100.00 / sum(coalesce(leads_count, 0)), 2)
-    end as lead_from_paid
-from big_tab
-group by 1
-having round(sum(leads_count) * 100.00 / sum(visitors_count), 2) !=0
-order by lead_from_paid desc;
+    round(
+        sum(leads_count) * 100.00 / sum(visitors_count), 2
+    ) AS visitor_from_lead,
+    CASE
+        WHEN sum(coalesce(leads_count, 0)) = 0 THEN 0.00 ELSE
+            round(
+                sum(coalesce(purchases_count, 0))
+                * 100.00
+                / sum(coalesce(leads_count, 0)),
+                2
+            )
+    END AS lead_from_paid
+FROM big_tab
+GROUP BY 1
+HAVING round(sum(leads_count) * 100.00 / sum(visitors_count), 2) != 0
+ORDER BY lead_from_paid DESC;
 
 -- Сколько мы тратим по разным каналам в динамике?
 /*aggregate_last_paid_click*/
@@ -155,25 +185,47 @@ select
     sum(coalesce(revenue, 0)) - sum(coalesce(total_cost, 0)) as payback
 from big_tab
 group by 1
-having sum(total_cost)>0 or sum(revenue)>0;
+having sum(total_cost) > 0 or sum(revenue) > 0;
 
 -- Сколько кликов на рекламу совершается и траты на рекламу по utm_campaign 
 with tab as (
-select source utm_source, campaign utm_campaign, count(*) count_click, null as daily_spent from sessions s
-group by 1, 2
+    select
+        source as utm_source,
+        campaign as utm_campaign,
+        count(*) as count_click,
+        null as daily_spent
+    from sessions
+    group by 1, 2
 ),
+
 tab2 as (
-select utm_source, utm_campaign, sum(daily_spent) daily_spent, null as count_click from ya_ads ya
-group by 1, 2
-union
-select utm_source, utm_campaign, sum(daily_spent) daily_spent, null as count_click from vk_ads va
-group by 1, 2
-order by 1
+    select
+        utm_source,
+        utm_campaign,
+        sum(daily_spent) as daily_spent,
+        null as count_click
+    from ya_ads
+    group by 1, 2
+    union
+    select
+        utm_source,
+        utm_campaign,
+        sum(daily_spent) as daily_spent,
+        null as count_click
+    from vk_ads
+    group by 1, 2
+    order by 1
 )
-select tab2.utm_source, tab2.utm_campaign, tab2.daily_spent, tab.count_click from tab
+
+select
+    tab2.utm_source,
+    tab2.utm_campaign,
+    tab2.daily_spent,
+    tab.count_click
+from tab
 inner join tab2
-on tab.utm_campaign = tab2.utm_campaign and tab.utm_source = tab2.utm_source
-order by 1, 3 desc
+    on tab.utm_campaign = tab2.utm_campaign and tab.utm_source = tab2.utm_source
+order by 1, 3 desc;
 
 -- Таблица с расчетами cpu/cpl/cppu/roi по utm_source
 -- /*aggregate_last_paid_click*/
@@ -182,16 +234,27 @@ select
     round(sum(coalesce(total_cost, 0)) / sum(visitors_count), 2) as cpu,
     round(sum(coalesce(total_cost, 0)) / sum(leads_count), 2) as cpl,
     round(sum(coalesce(total_cost, 0)) / sum(purchases_count), 2) as cppu,
-    round((sum(coalesce(revenue, 0)) - sum(coalesce(total_cost, 0))) * 100.00 / sum(coalesce(total_cost, 0)), 2) as roi
+    round(
+        (sum(coalesce(revenue, 0)) - sum(coalesce(total_cost, 0)))
+        * 100.00
+        / sum(coalesce(total_cost, 0)),
+        2
+    ) as roi
 from big_tab
 group by 1
-having sum(total_cost)>0;
+having sum(total_cost) > 0;
 
 -- Расчет среднего, минимального и максимального чека по формату обучения и дате
-select to_char(created_at, 'YYYY-MM-DD'), learning_format, avg(amount) avg_amount, min(amount) min_amount, max(amount) max_amount from leads
+select
+    to_char(created_at, 'YYYY-MM-DD'),
+    learning_format,
+    avg(amount) as avg_amount,
+    min(amount) as min_amount,
+    max(amount) as max_amount
+from leads
 where status_id = 142
 group by 1, learning_format
-order by 2, 1
+order by 2, 1;
     
 -- Через какое время после запуска компании маркетинг может анализировать компанию используя ваш дашборд?
 -- Можно посчитать за сколько дней с момента перехода по рекламе закрывается 90% лидов.
@@ -204,22 +267,32 @@ with tab as (
         l.created_at as created_at,
         row_number() over (partition by s.visitor_id order by s.visit_date desc)
         as rn
-    from sessions s
-    left join leads l
+    from sessions as s
+    left join leads as l
         on
             s.visitor_id = l.visitor_id
             and s.visit_date <= l.created_at
     where s.source in ('yandex', 'vk', 'telegram')
 )
+
 select
     utm_source,
     case
         when utm_source = 'yandex'
-        then percentile_disc(0.9) within group (order by created_at - visit_date)
+            then
+                percentile_disc(0.9) within group (
+                    order by created_at - visit_date
+                )
         when utm_source = 'vk'
-        then percentile_disc(0.9) within group (order by created_at - visit_date)
+            then
+                percentile_disc(0.9) within group (
+                    order by created_at - visit_date
+                )
         when utm_source = 'telegram'
-        then percentile_disc(0.9) within group (order by created_at - visit_date)        
+            then
+                percentile_disc(0.9) within group (
+                    order by created_at - visit_date
+                )
     end as percent_90_leads
 from tab
 where rn = 1
