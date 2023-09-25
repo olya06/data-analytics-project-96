536d1a42440e09d90d7b7e22401f7d0600581f87
Olga Okuneva <133260595+olya06@users.noreply.github.com>
2023-09-25 18:49:40 +0300

    dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -4,15 +4,15 @@ group by 1
 order by 1
 
 -- Какие каналы их приводят на сайт? Хочется видеть по дням/неделям/месяцам
-select medium, count(*), date_trunc('day', visit_date) visit_date from sessions s -- по дням
+select source, count(*), date_trunc('day', visit_date) visit_date from sessions s -- по дням
 group by 1, 3
 order by visit_date
 
-select medium, count(*), date_trunc('week', visit_date) visit_date from sessions s -- по неделям
+select source, count(*), date_trunc('week', visit_date) visit_date from sessions s -- по неделям
 group by 1, 3
 order by visit_date
 
-select medium, count(*), date_trunc('month', visit_date) visit_date from sessions s  -- по месяцам
+select source, count(*), date_trunc('month', visit_date) visit_date from sessions s  -- по месяцам
 group by 1, 3
 order by visit_date
 
