002e55e4d3433f3b38f484d0ccb7529c59602f26
Olga Okuneva <133260595+olya06@users.noreply.github.com>
2023-09-27 20:40:48 +0300

    dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -65,12 +65,13 @@ WITH last_paid_click AS (
     WHERE s.medium NOT IN ('organic')
 ),
 
-advertising AS (
+-- Траты на рекламу за 1 день
+count_buy AS (
     SELECT
-        date(visit_date) AS visit_date,
         utm_source,
         utm_medium,
         utm_campaign,
+        date(visit_date) AS visit_date,
         count(visitor_id) AS visitors_count,
         sum(CASE WHEN lead_id IS NOT null THEN 1 ELSE 0 END) AS leads_count,
         sum(
@@ -81,8 +82,7 @@ advertising AS (
                 ELSE 0
             END
         ) AS purchases_count,
-        sum(amount) AS revenue,
-        null AS total_cost
+        sum(amount) AS revenue
     FROM last_paid_click
     WHERE last_paid_click.rn = 1
     GROUP BY
@@ -90,56 +90,57 @@ advertising AS (
         utm_source,
         utm_medium,
         utm_campaign
-    UNION ALL
-    SELECT
-        campaign_date AS visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        null AS revenue,
-        null AS visitors_count,
-        null AS leads_count,
-        null AS purchases_count,
-        daily_spent AS total_cost
-    FROM vk_ads
-    UNION ALL
-    SELECT
-        campaign_date AS visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        null AS revenue,
-        null AS visitors_count,
-        null AS leads_count,
-        null AS purchases_count,
-        daily_spent AS total_cost
-    FROM ya_ads
 ),
 
-big_tab AS (
+advertising AS (
     SELECT
+        campaign_date AS visit_date,
         utm_source,
         utm_medium,
         utm_campaign,
-        date(visit_date) AS visit_date,
-        sum(visitors_count) AS visitors_count,
-        sum(total_cost) AS total_cost,
-        sum(leads_count) AS leads_count,
-        sum(purchases_count) AS purchases_count,
-        sum(revenue) AS revenue
-    FROM advertising
-    GROUP BY
-        visit_date,
+        sum(daily_spent) AS total_cost
+    FROM vk_ads
+    GROUP BY 1, 2, 3, 4
+    UNION ALL
+    SELECT
+        campaign_date AS visit_date,
         utm_source,
         utm_medium,
-        utm_campaign
-    ORDER BY
-        revenue DESC NULLS LAST,
-        visit_date ASC,
-        visitors_count DESC,
-        utm_source ASC,
-        utm_medium ASC,
-        utm_campaign ASC
+        utm_campaign,
+        sum(daily_spent) AS total_cost
+    FROM ya_ads
+    GROUP BY 1, 2, 3, 4
+)
+
+SELECT
+    cb.utm_source,
+    cb.utm_medium,
+    cb.utm_campaign,
+    date(cb.visit_date) AS visit_date,
+    sum(visitors_count) AS visitors_count,
+    sum(total_cost) AS total_cost,
+    sum(leads_count) AS leads_count,
+    sum(purchases_count) AS purchases_count,
+    sum(revenue) AS revenue
+FROM advertising AS a
+RIGHT JOIN count_buy AS cb
+    ON
+        a.visit_date = cb.visit_date
+        AND a.utm_source = cb.utm_source
+        AND a.utm_medium = cb.utm_medium
+        AND a.utm_campaign = cb.utm_campaign
+GROUP BY
+    cb.visit_date,
+    cb.utm_source,
+    cb.utm_medium,
+    cb.utm_campaign
+ORDER BY
+    revenue DESC NULLS LAST,
+    visit_date ASC,
+    visitors_count DESC,
+    utm_source ASC,
+    utm_medium ASC,
+    utm_campaign ASC
 )
 
 SELECT
